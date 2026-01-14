5ea81f291f9e502e2c37586b02c44361f9a3e48e
Rutam Bhagat <rutambhagat@gmail.com>
2026-01-14 18:20:50 +0530

    feat: define database schema and relations for users, posts, and comments

--- packages/db/src/relations.ts
+++ packages/db/src/relations.ts
@@ -1,4 +1,26 @@
 import * as schema from "./schema";
 import { defineRelations } from "drizzle-orm";
 
-export const relations = defineRelations(schema, (r) => ({}));
+export const relations = defineRelations(schema, (r) => ({
+  usersTable: {
+    posts: r.many.postsTable(),
+    comments: r.many.commentsTable(),
+  },
+  postsTable: {
+    user: r.one.usersTable({
+      from: r.postsTable.userId,
+      to: r.usersTable.id,
+    }),
+    comments: r.many.commentsTable(),
+  },
+  commentsTable: {
+    post: r.one.postsTable({
+      from: r.commentsTable.postId,
+      to: r.postsTable.id,
+    }),
+    user: r.one.usersTable({
+      from: r.commentsTable.userId,
+      to: r.usersTable.id,
+    }),
+  },
+}));
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,1 +1,28 @@
-export const schema = {};
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+const timestamps = {
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp(),
+};
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar({ length: 30 }).notNull().unique(),
+  ...timestamps,
+});
+
+export const postsTable = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  contents: varchar().notNull(),
+  url: varchar().notNull(),
+  userId: integer().references(() => usersTable.id, { onDelete: "cascade" }),
+  ...timestamps,
+});
+
+export const commentsTable = pgTable("comments", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  contents: varchar({ length: 240 }).notNull(),
+  postId: integer().references(() => postsTable.id, { onDelete: "cascade" }),
+  userId: integer().references(() => usersTable.id, { onDelete: "cascade" }),
+  ...timestamps,
+});
