6e95e133810abb2c25fb27878afd77e42abcdae0
Rutam Bhagat <72187009+RutamBhagat@users.noreply.github.com>
2026-01-18 01:26:09 +0530

    feat: add simple cte tag (#5)
    simple cte

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,7 +1,7 @@
 import { env } from "@instagram/env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { relations } from "./relations";
-export { sql, asc, desc, eq, count, countDistinct } from "drizzle-orm";
+export { sql, asc, desc, eq, lte, gte, count, countDistinct, and, or } from "drizzle-orm";
 
 export const db = drizzle(env.DATABASE_URL, {
   relations,
